25acf697e3307d1e8bf3979ba3010f09a5321e28
Jacobus Raubenheimer <j.e.raubenheimer@gmail.com>
2020-01-29 16:01:11 +0200

    Bare Bones

---
+++ App.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font';
+import Home from './screens/home';
+import Navigator from './routes/homeStack';
+
+const getFonts = () => Font.loadAsync({
+    'source-regular': require('./assets/fonts/SourceSansPro-Regular.ttf'),
+    'source-bold' : require('./assets/fonts/SourceSansPro-Bold.ttf'),
+    'source-italic' : require('./assets/fonts/SourceSansPro-Italic.ttf')
+  });
+
+export default function App() {
+  const[fontsLoaded, setFontsLoaded] = useState(false);
+
+  if(fontsLoaded){
+    return (
+     <Navigator />
+    );
+  } else {
+    return (
+    <AppLoading
+      startAsync={getFonts}
+      onFinish={()=> setFontsLoaded(true)}
+    />
+    )
+  }
+  
+}
+
