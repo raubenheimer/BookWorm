8d3ced4147c57593a0141a0cb99fbd0254a84eb3
Jacobus Raubenheimer <j.e.raubenheimer@gmail.com>
2020-01-30 17:57:48 +0200

    added rating images

--- screens/home.js
+++ screens/home.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
 import { StyleSheet, View, Text, Button, FlatList, TouchableOpacity } from 'react-native';
 import { globalStyles } from '../styles/global';
-
+import Card from '../shared/card';
 
 
 export default function Home({ navigation }) {
 
     const [reviews, setReviews] = useState([
         { title: 'Catch 22', rating: 5, body: 'helo', key: '1' },
-        { title: 'The Old Man and the Sea', rating: 5, body: 'helo', key: '2' },
-        { title: '12 Rules for Life', rating: 5, body: 'helo', key: '3' },
+        { title: 'The Old Man and the Sea', rating: 4, body: 'helo', key: '2' },
+        { title: '12 Rules for Life', rating: 3, body: 'helo', key: '3' },
     ])
     const pressHandler = () => {
         navigation.navigate('ReviewDetails')
@@ -21,7 +21,9 @@ export default function Home({ navigation }) {
                 data={reviews}
                 renderItem={({ item }) => (
                     <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails', item)}>
-                        <Text style={globalStyles.titleText}>{item.title}</Text>
+                        <Card>
+                            <Text style={globalStyles.titleText}>{item.title}</Text>
+                        </Card>
                     </TouchableOpacity>
                 )}
             />
--- styles/global.js
+++ styles/global.js
@@ -15,3 +15,13 @@ export const globalStyles = StyleSheet.create({
         lineHeight: 20,
     }
 });
+
+export const images = {
+    ratings: {
+        '1' : require('../assets/rating1.png'),
+        '2' : require('../assets/rating2.png'),
+        '3' : require('../assets/rating3.png'),
+        '4' : require('../assets/rating4.png'),
+        '5' : require('../assets/rating5.png')
+    }
+}
